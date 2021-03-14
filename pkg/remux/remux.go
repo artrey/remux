223efc262f43c829d84f97e1e5e48c16b747bf19
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2021-03-14 15:49:23 +0300

    Added regex path

--- pkg/remux/remux.go
+++ pkg/remux/remux.go
@@ -1,8 +1,10 @@
 package remux
 
 import (
+	"context"
 	"errors"
 	"net/http"
+	"regexp"
 	"strings"
 	"sync"
 )
@@ -10,6 +12,7 @@ import (
 type ReMux struct {
 	mu              sync.RWMutex
 	plain           map[string]map[string]http.Handler         // method-path-handler
+	regex           map[string]map[*regexp.Regexp]http.Handler // method-regexPath-handler
 	notFoundHandler http.Handler
 }
 
@@ -18,6 +21,16 @@ func New() *ReMux {
 		notFoundHandler: http.HandlerFunc(defaultNotFoundHandler),
 	}
 }
+
+type contextKey struct {
+	name string
+}
+
+func (c *contextKey) String() string {
+	return c.name
+}
+
+var paramsContextKey = &contextKey{"remux context key for params"}
 
 var defaultNotFoundHandler = func(writer http.ResponseWriter, request *http.Request) {
 	writer.WriteHeader(http.StatusNotFound)
@@ -28,7 +41,13 @@ var (
 	ErrInvalidPath      = errors.New("invalid path")
 	ErrNilHandler       = errors.New("handler is nil")
 	ErrAmbiguousMapping = errors.New("ambiguous mapping")
+	ErrNoParams         = errors.New("no params")
 )
+
+type Params struct {
+	Named      map[string]string
+	Positional []string
+}
 
 func (r *ReMux) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	r.mu.RLock()
@@ -37,6 +56,30 @@ func (r *ReMux) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	if handlers, exists := r.plain[request.Method]; exists {
 		if h, ok := handlers[request.URL.Path]; ok {
 			handler = h
+		}
+	}
+	if handler == nil {
+		if handlers, exists := r.regex[request.Method]; exists {
+			for path, h := range handlers {
+				if matches := path.FindStringSubmatch(request.URL.Path); matches != nil {
+					params := &Params{
+						Named:      make(map[string]string),
+						Positional: matches[1:],
+					}
+					for index, name := range path.SubexpNames() {
+						if name == "" {
+							continue
+						}
+						params.Named[name] = matches[index]
+					}
+
+					ctx := context.WithValue(request.Context(), paramsContextKey, params)
+					request = request.WithContext(ctx)
+
+					handler = h
+					break
+				}
+			}
 		}
 	}
 	if handler == nil {
@@ -87,6 +130,46 @@ func (r *ReMux) RegisterPlain(method, path string, handler http.Handler) error {
 
 	r.plain[method][path] = handler
 	return nil
+}
+
+func (r *ReMux) RegisterRegex(method string, path *regexp.Regexp, handler http.Handler) error {
+	if !isValidMethod(method) {
+		return ErrInvalidMethod
+	}
+	if !strings.HasPrefix(path.String(), "^/") {
+		return ErrInvalidPath
+	}
+	if !strings.HasSuffix(path.String(), "$") {
+		return ErrInvalidPath
+	}
+	if handler == nil {
+		return ErrNilHandler
+	}
+
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if _, exists := r.regex[method][path]; exists {
+		return ErrAmbiguousMapping
+	}
+
+	if r.regex == nil {
+		r.regex = make(map[string]map[*regexp.Regexp]http.Handler)
+	}
+	if r.regex[method] == nil {
+		r.regex[method] = make(map[*regexp.Regexp]http.Handler)
+	}
+
+	r.regex[method][path] = handler
+	return nil
+}
+
+func PathParams(ctx context.Context) (*Params, error) {
+	params, ok := ctx.Value(paramsContextKey).(*Params)
+	if !ok {
+		return nil, ErrNoParams
+	}
+	return params, nil
 }
 
 var validMethods = []string{
--- pkg/remux/remux_test.go
+++ pkg/remux/remux_test.go
@@ -1,17 +1,24 @@
 package remux_test
 
 import (
+	"bytes"
 	"github.com/artrey/remux/pkg/remux"
 	"net/http"
 	"net/http/httptest"
+	"regexp"
 	"testing"
 )
 
-func stubHandler(w http.ResponseWriter, r *http.Request) {}
+func stubHandler(w http.ResponseWriter, r *http.Request) {
+	params, err := remux.PathParams(r.Context())
+	if err == nil {
+		_, _ = w.Write([]byte(params.Named["id"]))
+	}
+}
 
 func TestReMux_RegisterPlain(t *testing.T) {
-	ambiguousMappingMux := remux.New()
-	err := ambiguousMappingMux.RegisterPlain(http.MethodGet, "/test", http.HandlerFunc(stubHandler))
+	muxWithHandlers := remux.New()
+	err := muxWithHandlers.RegisterPlain(http.MethodGet, "/test", http.HandlerFunc(stubHandler))
 	if err != nil {
 		t.Errorf("mux problem: %v", err)
 		return
@@ -29,11 +36,21 @@ func TestReMux_RegisterPlain(t *testing.T) {
 		want error
 	}{
 		{
-			name: "correct register",
+			name: "correct register on empty mux",
 			args: args{
 				mux:     remux.New(),
 				method:  http.MethodGet,
 				path:    "/test",
+				handler: http.HandlerFunc(stubHandler),
+			},
+			want: nil,
+		},
+		{
+			name: "correct register on mux with handlers",
+			args: args{
+				mux:     muxWithHandlers,
+				method:  http.MethodGet,
+				path:    "/test2",
 				handler: http.HandlerFunc(stubHandler),
 			},
 			want: nil,
@@ -68,7 +85,7 @@ func TestReMux_RegisterPlain(t *testing.T) {
 		{
 			name: "ambiguous mapping",
 			args: args{
-				mux:     ambiguousMappingMux,
+				mux:     muxWithHandlers,
 				method:  http.MethodGet,
 				path:    "/test",
 				handler: http.HandlerFunc(stubHandler),
@@ -82,6 +99,203 @@ func TestReMux_RegisterPlain(t *testing.T) {
 			got := tt.args.mux.RegisterPlain(tt.args.method, tt.args.path, tt.args.handler)
 			if tt.want != got {
 				t.Errorf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestReMux_RegisterRegex(t *testing.T) {
+	muxWithHandlers := remux.New()
+
+	regexps := make([]*regexp.Regexp, 0)
+	for _, rePattern := range []string{
+		`^/test/(?P<id>\d+)$`,
+		`^/test/(?P<id>\d+)/subtest$`,
+		`/test/(?P<id>\d+)$`,
+		`^test/(?P<id>\d+)$`,
+		`^/test/(?P<id>\d+)`,
+		`test/(?P<id>\d+)`,
+	} {
+		re, err := regexp.Compile(rePattern)
+		if err != nil {
+			t.Errorf("regexp.Compile problem: %v", err)
+			return
+		}
+		regexps = append(regexps, re)
+	}
+	err := muxWithHandlers.RegisterRegex(http.MethodGet, regexps[0], http.HandlerFunc(stubHandler))
+	if err != nil {
+		t.Errorf("mux problem: %v", err)
+		return
+	}
+
+	type args struct {
+		mux     *remux.ReMux
+		method  string
+		path    *regexp.Regexp
+		handler http.Handler
+	}
+	tests := []struct {
+		name string
+		args args
+		want error
+	}{
+		{
+			name: "correct register on empty mux",
+			args: args{
+				mux:     remux.New(),
+				method:  http.MethodGet,
+				path:    regexps[0],
+				handler: http.HandlerFunc(stubHandler),
+			},
+			want: nil,
+		},
+		{
+			name: "correct register on mux with handlers",
+			args: args{
+				mux:     muxWithHandlers,
+				method:  http.MethodGet,
+				path:    regexps[1],
+				handler: http.HandlerFunc(stubHandler),
+			},
+			want: nil,
+		},
+		{
+			name: "invalid method",
+			args: args{
+				mux:    remux.New(),
+				method: "foo",
+			},
+			want: remux.ErrInvalidMethod,
+		},
+		{
+			name: "invalid path ^",
+			args: args{
+				mux:    remux.New(),
+				method: http.MethodGet,
+				path:   regexps[2],
+			},
+			want: remux.ErrInvalidPath,
+		},
+		{
+			name: "invalid path /",
+			args: args{
+				mux:    remux.New(),
+				method: http.MethodGet,
+				path:   regexps[3],
+			},
+			want: remux.ErrInvalidPath,
+		},
+		{
+			name: "invalid path $",
+			args: args{
+				mux:    remux.New(),
+				method: http.MethodGet,
+				path:   regexps[4],
+			},
+			want: remux.ErrInvalidPath,
+		},
+		{
+			name: "invalid path ^/$",
+			args: args{
+				mux:    remux.New(),
+				method: http.MethodGet,
+				path:   regexps[5],
+			},
+			want: remux.ErrInvalidPath,
+		},
+		{
+			name: "invalid handler",
+			args: args{
+				mux:     remux.New(),
+				method:  http.MethodGet,
+				path:    regexps[0],
+				handler: nil,
+			},
+			want: remux.ErrNilHandler,
+		},
+		{
+			name: "ambiguous mapping",
+			args: args{
+				mux:     muxWithHandlers,
+				method:  http.MethodGet,
+				path:    regexps[0],
+				handler: http.HandlerFunc(stubHandler),
+			},
+			want: remux.ErrAmbiguousMapping,
+		},
+	}
+	for _, test := range tests {
+		tt := test
+		t.Run(test.name, func(t *testing.T) {
+			got := tt.args.mux.RegisterRegex(tt.args.method, tt.args.path, tt.args.handler)
+			if tt.want != got {
+				t.Errorf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPathParams(t *testing.T) {
+	mux := remux.New()
+
+	re, err := regexp.Compile(`^/test/(?P<id>\d+)$`)
+	if err != nil {
+		t.Errorf("regexp.Compile problem: %v", err)
+		return
+	}
+	err = mux.RegisterRegex(http.MethodGet, re, http.HandlerFunc(stubHandler))
+	if err != nil {
+		t.Errorf("mux problem: %v", err)
+		return
+	}
+	err = mux.RegisterPlain(http.MethodGet, "/test", http.HandlerFunc(stubHandler))
+	if err != nil {
+		t.Errorf("mux problem: %v", err)
+		return
+	}
+
+	type args struct {
+		mux    *remux.ReMux
+		method string
+		path   string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+		{
+			name: "named agr found",
+			args: args{
+				mux:    mux,
+				method: http.MethodGet,
+				path:   "/test/2",
+			},
+			want: []byte("2"),
+		},
+		{
+			name: "named agr not found",
+			args: args{
+				mux:    mux,
+				method: http.MethodGet,
+				path:   "/test",
+			},
+			want: []byte{},
+		},
+	}
+	for _, test := range tests {
+		tt := test
+		t.Run(test.name, func(t *testing.T) {
+			request := httptest.NewRequest(tt.args.method, tt.args.path, nil)
+			response := httptest.NewRecorder()
+			tt.args.mux.ServeHTTP(response, request)
+			if response.Code != 200 {
+				t.Errorf("error response code: %v", response.Code)
+			}
+			got := response.Body.Bytes()
+			if !bytes.Equal(tt.want, got) {
+				t.Errorf("got %s, want %s", got, tt.want)
 			}
 		})
 	}
